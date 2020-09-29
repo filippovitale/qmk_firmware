7a95f9fabcf5e9865244534173a02448779d9ed8
Filippo Vitale <fvitale@arbor.net>
2020-09-29 14:26:52 +0200

    adapt lily58 keymap to crkbd

--- keyboards/crkbd/keymaps/filippovitale/rules.mk
+++ keyboards/crkbd/keymaps/filippovitale/rules.mk
@@ -1,4 +1,12 @@
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight
+# BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+
+# BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+# MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+# MIDI_ENABLE = no            # MIDI controls
+# AUDIO_ENABLE = no           # Audio output on port C6
+# BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+# SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
